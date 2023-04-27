c2b836ef4891ec0ea35f667d100361ec47aacee6
Isaac Boakye <isaacboakye77@outlook.com>
2023-04-26 17:32:23 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -33,40 +33,17 @@ void intHdle(int x)
 int _printf(const char *fmt, ...)
 {
 	va_list lst;
-	char a, *b;
 	int i = 0;
 
 	va_start(lst, fmt);
+	
 	if (!(fmt && *fmt))
 		return (-1);
-	while (fmt != NULL)
+	while (*fmt != '\0')
 	{
-		if (*fmt != '%')
-		{
-			i++;
-			write(1, fmt, 1);
-			fmt++;
-		} else
-		{
-			fmt = fmt + 1;
-			if (*fmt == 'c')
-			{
-				a = va_arg(lst, int);
-				write(1, &a, 1);
-			} else if (*fmt == 's')
-			{
-				b = va_arg(lst, char *);
-				write(1, &b, strlen(b));
-			} else if (*fmt == '%')
-			{
-				a = '%';
-				write(1, &a, 1);
-			} else if (*fmt == 'd')
-			{
-				intHdle(va_arg(lst, int));
-			}
-			i = i + 2;
-		}
+		fmt++;
+		i++;
 	}
+	vprintf((fmt - i), lst);
 	return (i);
 }
